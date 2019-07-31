1ae5cf6c19d4d50675180a112f7d5ba863e6e058
Mfedal <46532947+Mfedal@users.noreply.github.com>
2019-07-31 09:53:53 -0400

    Update MigrationGenerator.php

--- src/SQLGen/MigrationGenerator.php
+++ src/SQLGen/MigrationGenerator.php
@@ -4,13 +4,20 @@
 class MigrationGenerator {
 
     public static function generate($diffs, $method) {
+        global $foreign_keys_list, $disable_fk_list;
+
         $sql = "";
+        $foreign_keys_list=array();
+        $disable_fk_list=array();
+
         foreach ($diffs as $diff) {
             $reflection = new \ReflectionClass($diff);
             $sqlGenClass = __NAMESPACE__."\\DiffToSQL\\".$reflection->getShortName()."SQL";
             $gen = new $sqlGenClass($diff);
             $sql .= $gen->$method()."\n";
         }
+        $foreign_keys_script = implode("\n", $foreign_keys_list);
+        $sql .= $foreign_keys_script;
         return $sql;
     }
 
